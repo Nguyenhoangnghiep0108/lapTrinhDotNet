5d15bc42e62c5fb9df47596a582e247334bc9c90
NguyenNghiep <nguyennghiep871@gmail.com>
2025-09-11 23:53:31 +0700

    Hang so const

---
+++ Bai2_Hangso/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Bai2_Hangso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hang so la đại lượng không thể thay dổi const 
+            const float pI = 3.14f;
+            const float NhietDoSoi = 100;
+            const float NhietDoDong = 0;
+
+        }
+    }
+}
