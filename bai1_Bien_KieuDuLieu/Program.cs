91ee42eff0ec93fb46dea3651ad44e2e007e0798
NguyenNghiep <nguyennghiep871@gmail.com>
2025-09-11 22:37:03 +0700

    kieu du lieu double and var

--- bai1_Bien_KieuDuLieu/Program.cs
+++ bai1_Bien_KieuDuLieu/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace bai1_Bien_KieuDuLieu
+﻿using System.Text;
+
+namespace bai1_Bien_KieuDuLieu
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Giúp hiện chữ tiếng việt không lỗi font
+            Console.OutputEncoding = Encoding.UTF8;
+            //Khai báo biến a b c kiểu double
+            double a, b, c;
+            //Khai bao  biến kiểu dữ liệu kiểu var lưu ý khi khai báo phải có đi kèm giá trị
+            var d = 10;
+            //Nhập  giá trị của a b c Dùng hàm parse vì readline nhận giá trị chuổi
+            Console.WriteLine("Nhap gia tri a b c : ");
+            a = double.Parse(Console.ReadLine());
+            c = double.Parse(Console.ReadLine());
+            c = double.Parse(Console.ReadLine());
+
         }
     }
 }
