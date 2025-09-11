3cf8654a590c853361bead84e3c52a1ae6318e1f
NguyenNghiep <nguyennghiep871@gmail.com>
2025-09-11 21:50:24 +0700

    Add project files.

---
+++ bai1_Bien_KieuDuLieu/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace bai1_Bien_KieuDuLieu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
