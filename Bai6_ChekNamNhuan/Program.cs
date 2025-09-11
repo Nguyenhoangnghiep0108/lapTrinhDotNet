0e7a2eb60b96d51cb6de3b67f64936f14f5aec44
NguyenNghiep <nguyennghiep871@gmail.com>
2025-09-12 02:35:23 +0700

    Chek nham nhuan

---
+++ Bai6_ChekNamNhuan/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Bai6_ChekNamNhuan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chek Nam Nhuan");
+            long nam;
+            Console.WriteLine("Nhap Nam Can Chek");
+            nam = long.Parse(Console.ReadLine());
+            if (nam % 4 == 0 && nam % 100 != 0 || nam % 400 == 0)
+            {
+                Console.WriteLine($"Nam {nam} la nam nhuan");
+            }
+            else 
+            {
+                Console.WriteLine($"Nam {nam} khong phai la nam nhuan");
+            }
+        }
+    }
+}
