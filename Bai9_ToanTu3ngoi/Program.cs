8f9a80cf195af629ceda4df8df5c221e8b6461eb
NguyenNghiep <nguyennghiep871@gmail.com>
2025-09-14 05:07:37 +0700

    toan tu 3 ngoi

---
+++ Bai9_ToanTu3ngoi/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Bai9_ToanTu3ngoi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dtb;
+            Console.WriteLine("Vui long nhap diem trung binh");
+             dtb = double.Parse(Console.ReadLine());
+            string kq = (dtb > 8) ? "Gioi" : (dtb < 8 && dtb > 6.5) ? "Kha" : "Yeu";
+            Console.WriteLine(kq);
+        }
+    }
+}
