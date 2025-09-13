e71a5fb075d8f2a15fec684781c8d2185aadec44
NguyenNghiep <nguyennghiep871@gmail.com>
2025-09-14 05:40:06 +0700

    Viet menu bang switch case co ban

---
+++ Bai10_SwitchCase/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Bai10_SwitchCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap vao 1 so de kiem tra chan le ");
+            int soCanKiemTra;
+            soCanKiemTra = int.Parse(Console.ReadLine());
+            int chanle = soCanKiemTra % 2;
+            switch (chanle)
+            {
+                case 0:
+                    Console.WriteLine("Day la so chan");
+                    break;
+                default:
+                    Console.WriteLine("Day la so le");
+                    break;
+            }
+        }
+    }
+}
---
+++ Bai11_XayDungMenuLuaChon_SwitchCase/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Bai11_XayDungMenuLuaChon_SwitchCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chao ban day la menu lua chon thao tac\n" +
+                "1.Tim theo ten \n" +
+                "2.Tim theo tac gia\n" +
+                "3.Tim theo nha xuat ban\n" +
+                "4.Tim theo tieu de\n" +
+                "5.Thoat");
+            int bienNhap;
+            bienNhap = int.Parse(Console.ReadLine());
+            switch (bienNhap)
+            {
+                case 1:
+                    Console.WriteLine("Ban chon thao tac tim theo ten");
+                    break;
+                case 2:
+                    Console.WriteLine("Day la thao tac tim theo tac gia");
+                    break;
+                case 3:
+                    Console.WriteLine("Day la thao tac tiem theo nha xuat ban");
+                    break;
+                case 4:
+                    Console.WriteLine("Day la thao tac tiem theo tieu de");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
