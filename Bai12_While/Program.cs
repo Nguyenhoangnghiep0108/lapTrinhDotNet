45df095a70e5a2afa18fb2a0c993fbebe88cbf45
NguyenNghiep <nguyennghiep871@gmail.com>
2025-09-14 05:55:15 +0700

    dung while de tao menu nhap

---
+++ Bai12_While/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Bai12_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bienSo;
+            Console.WriteLine("Vui long nhap so tu 1 den 99");
+            bienSo = int.Parse(Console.ReadLine());
+            while(bienSo < 1 || bienSo > 99)
+            {
+                Console.WriteLine($"Ban da nhap so vuoc gioi han cho phep {bienSo}");
+                bienSo = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"Ban da nhap so {bienSo}");
+        }
+    }
+}
