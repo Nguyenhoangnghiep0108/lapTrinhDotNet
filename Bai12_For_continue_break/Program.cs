96cdf2a3f5f640ca22871b3edf5e272f1fa0b05a
NguyenNghiep <nguyennghiep871@gmail.com>
2025-09-17 00:06:31 +0700

    for àn foreach

---
+++ Bai12_For_continue_break/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Bai12_For_continue_break
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //for (Khởi tạo; diều kiện dừng; bước nhảy) {
+            //    khối lệnh
+            //        }
+            for(int i = 0; i < 9; i++){
+           Console.WriteLine("abc");
+            }
+            int[] arr= { 1,2,3,4,5 ,6 };
+            foreach(int i in arr)
+            {
+                Console.WriteLine(" " +i);
+            }
+
+        }
+    }
+}
