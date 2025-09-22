f44a8e9d857732e73387db798b4c43d741b69d00
NguyenNghiep <nguyennghiep871@gmail.com>
2025-09-22 23:39:20 +0700

    Try catsh finally

---
+++ Bai17_Funtion/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Bai17_Funtion
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        static int tinhtong(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+        static int daythua(int a)
+        {
+            int tich = 1;
+            for (int i = a; i > 1; i--)
+            {
+                tich *= i; 
+            }
+            return tich;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int a = tinhtong(1, 2, 3);
+            Console.WriteLine(a);
+            int b = daythua(5);
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ Bai19_Try_Cat_Finally/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace Bai19_Try_Cat_Finally
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.OutputEncoding = Encoding.UTF8;
+                StringInfo s = Console.ReadLine();
+                DateTime dateTime = dateTime.Parse(s);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Vui long nhap lai du lieu ");
+
+            }
+            finally
+            {
+            }
+        }
+    }
+}
